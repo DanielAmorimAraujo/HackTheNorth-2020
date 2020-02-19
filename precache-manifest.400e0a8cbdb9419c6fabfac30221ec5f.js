f6ba92e80f144166684818377f0e22e5ebf64f08
Daniel Araujo <daniel.amorim.araujo@gmail.com>
2020-02-18 22:55:24 -0500

    Updates

---
+++ precache-manifest.400e0a8cbdb9419c6fabfac30221ec5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "393b118113cd1722a4e69daed6f2acef",
+    "url": "/HackTheNorth-2020/index.html"
+  },
+  {
+    "revision": "be71e65c9c1d18942800",
+    "url": "/HackTheNorth-2020/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "be71e65c9c1d18942800",
+    "url": "/HackTheNorth-2020/static/js/2.727c195a.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/HackTheNorth-2020/static/js/2.727c195a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7698a68ede4c799b069",
+    "url": "/HackTheNorth-2020/static/js/main.126de923.chunk.js"
+  },
+  {
+    "revision": "a1634f108d717507209b",
+    "url": "/HackTheNorth-2020/static/js/runtime-main.f3a5f2f6.js"
+  },
+  {
+    "revision": "54d9348c275bbf46af1a48cc1338f49d",
+    "url": "/HackTheNorth-2020/static/media/icon-dark.54d9348c.svg"
+  }
+]);
