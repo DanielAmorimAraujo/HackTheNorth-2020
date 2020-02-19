9d7e0ce817be411a1bc88080d47feb41ad4d4515
Daniel Araujo <daniel.amorim.araujo@gmail.com>
2020-02-18 22:42:31 -0500

    stitched Login and Profile together in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Login from 'sections/Login';
+import Profile from 'sections/Profile';
+
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: Inter, -apple-system, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif;
+    color: rgb(24, 50, 73);
+    background-color: rgb(242,  249, 251);
+    height: 100%;
+    padding: 25px 15px;
+  }
+`;
 
 const App = () => {
-  return <></>;
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  return (
+    <>
+      <GlobalStyle />
+      {!loggedIn ? <Login setLoggedIn={setLoggedIn} /> : <Profile setLoggedIn={setLoggedIn} />}
+    </>
+  );
 }
 
 export default App;
